1c81b9bd5bc013df8f9f8df0d125fc3f91a573eb
Eric Chen <eric.chen.us@gmail.com>
2023-02-07 19:00:23 -0500

    finsihed wk2 hw

--- week2/contracts/PennFT.sol
+++ week2/contracts/PennFT.sol
@@ -9,12 +9,33 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract PennFT is ERC721 {
     // todo: think about what variables you need to store the NFT metadata
+    string private _name;
+    string private _symbol;
+
+    //map from token id to token uri
+    mapping(uint256 => string) private _tokenURIs;
+    //map from token uri to owner
+    mapping(string => address) private _tokenURItoOwner;
+    //map from token uri to token id
+    mapping(string => uint256) private _tokenURItoID;
+    //map from token id to owner
+    mapping(uint256 => address) private _tokenIDtoOwner;
+    //map from owner to token id
+    mapping(address => uint256) private _ownerToTokenID;
+
+    //list of all token ids
+    uint256[] private _tokenIDs;
 
     /**
         The constructor for the PennFT contract. Passes the name and symbol to the ERC721 constructor.
     */
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
         // todo: initialize any variables you created here
+        _name = _name;
+        _symbol = _symbol;
     }
 
     /**
@@ -23,25 +44,49 @@ contract PennFT is ERC721 {
         @param tokenURI The tokenURI of the NFT
         @return The ID of the newly minted NFT
     */
-    function mintNFT(address recipient, string memory tokenURI) public returns (uint256)
-    {
+    function mintNFT(
+        address recipient,
+        string memory tokenURI
+    ) public returns (uint256) {
         // todo: finish this code
+
+        //create new id
+        uint256 new_id = _tokenIDs.length + 1;
+        //add new id to list of token ids
+        _tokenIDs.push(new_id);
+        //add token uri to map from token id to token uri
+        _tokenURIs[new_id] = tokenURI;
+        //add token uri to map from token uri to owner
+        _tokenURItoOwner[tokenURI] = recipient;
+        //add token uri to map from token uri to token id
+        _tokenURItoID[tokenURI] = new_id;
+        //add token id to map from token id to owner
+        _tokenIDtoOwner[new_id] = recipient;
+        //add owner to map from owner to token id
+        _ownerToTokenID[recipient] = new_id;
+
+        _mint(recipient, new_id);
     }
 
     /**
-    * Gets the URI of the specified token.
-    * @param tokenId uint256 ID of the token to query the URI of
-    * @return string URI of the token
-    */
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+     * Gets the URI of the specified token.
+     * @param tokenId uint256 ID of the token to query the URI of
+     * @return string URI of the token
+     */
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
         // todo: finish this code
+        return _tokenURIs[tokenId];
     }
 
     /**
-    * Gets the number of tokens in existence.
-    * @return uint256 representing the number of tokens in existence
-    */
+     * Gets the number of tokens in existence.
+     * @return uint256 representing the number of tokens in existence
+     */
     function tokenCount() public view returns (uint256) {
         // todo: finish this code
+        // iterate through the tokenIDs list and return the number of tokens
+        return _tokenIDs.length;
     }
 }
--- week2/scripts/MintPennFT.js
+++ week2/scripts/MintPennFT.js
@@ -1,37 +1,46 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 
-const TOKEN_ADDRESS = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
-const TOKEN_URI = "ipfs://QmPYXAvt8WzsmEytuSfsPrTLwqdNXPKmmF5VsJvvzPkTWW";
+const TOKEN_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const TOKEN_URI = 'ipfs://Qmc4oyxRFkFE3XM5BeZmtDDi4eiXGm4pxCZTaBtVZh38EB';
 
 async function main() {
-  // Load the contract to get its ABI
-  const contract = require("../artifacts/contracts/PennFT.sol/PennFT.json");
+    // Load the contract to get its ABI
+    const contract = require('../artifacts/contracts/PennFT.sol/PennFT.json');
 
-  // Get a reference to the deployed contract
-  const PennFT = await hre.ethers.getContractAt(contract.abi, TOKEN_ADDRESS);
+    // Get a reference to the deployed contract
+    const PennFT = await hre.ethers.getContractAt(contract.abi, TOKEN_ADDRESS);
 
-  // Get the available (pre-seeded) hardhat wallets
-  const wallets = await hre.ethers.provider.listAccounts();
+    // Get the available (pre-seeded) hardhat wallets
+    const wallets = await hre.ethers.provider.listAccounts();
 
-  // Just use the first one
-  const wallet = await hre.ethers.provider.getSigner(wallets[0]);
+    // Just use the first one
+    const wallet = await hre.ethers.provider.getSigner(wallets[0]);
 
-  // Mint a new NFT
-  const tx = await PennFT.functions.mintNFT(wallet.getAddress(), TOKEN_URI);
-  console.log(`Minted NFT with tx hash: ${tx.hash}`);
+    // Mint a new NFT
+    const tx = await PennFT.functions.mintNFT(wallet.getAddress(), TOKEN_URI);
+    console.log(`Minted NFT with tx hash: ${tx.hash}`);
 
-  // todo: mint another NFT to a different wallet
+    // todo: mint another NFT to a different wallet
+    const wallet2 = await hre.ethers.provider.getSigner(wallets[1]);
+    const tx2 = await PennFT.functions.mintNFT(wallet2.getAddress(), TOKEN_URI);
+    console.log(`Minted NFT with tx hash: ${tx2.hash}`);
 
-  // todo: get the number of NFTs minted
+    // todo: get the number of NFTs minted
+    const numNFTs = await PennFT.functions.tokenCount();
+    console.log(`Number of NFTs minted: ${numNFTs}`);
 
-  // todo: get the URI of the first NFT
+    // todo: get the URI of the first NFT
+    const uri = await PennFT.functions.tokenURI(1);
+    console.log(`URI of the first NFT: ${uri}`);
 
-  // todo: get the URI of the second NFT
+    // todo: get the URI of the second NFT
+    const uri2 = await PennFT.functions.tokenURI(2);
+    console.log(`URI of the second NFT: ${uri2}`);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
